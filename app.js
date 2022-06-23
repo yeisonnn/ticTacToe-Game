164ca51071e050436f638b325258d65fc65ce0af
yeisson giraldo <yeisonnn@gmail.com>
2022-06-23 18:44:17 -0500

    first layout, almost all functions and basic CSS

--- app.js
+++ app.js
@@ -3,14 +3,28 @@ const gameState = {
   board: [
     ['o', '1', '3'],
     ['o', 'o', '2'],
-    ['o', '3', '4']
-  ]
-}
+    ['o', '3', '4'],
+  ],
+};
 
 /*========================================================
 ----------------------------------------------------------
 =========================================================*/
 
+function createBoard(rows, cols) {
+  const board = [];
+  const column = new Array(cols);
+  column.fill(null);
+  for (let i = 0; i < rows; i++) {
+    board.push(column);
+  }
+  console.log(board);
+  return board;
+}
+
+/*========================================================
+----------------------------------------------------------
+=========================================================*/
 
 function getRow(gameState, rowIndex) {
   const searchedRow = gameState.board[rowIndex];
@@ -18,12 +32,11 @@ function getRow(gameState, rowIndex) {
   return searchedRow;
 }
 
-
 /*========================================================
 ----------------------------------------------------------
 =========================================================*/
 
-function getColumn(gameState,columnIndex) {
+function getColumn(gameState, columnIndex) {
   const searchedColumn = [];
   for (let column of gameState.board) {
     searchedColumn.push(column[columnIndex]);
@@ -32,46 +45,46 @@ function getColumn(gameState,columnIndex) {
   return searchedColumn;
 }
 
-
 /*========================================================
 ----------------------------------------------------------
 =========================================================*/
 
-function getDiagonalLeftToRight(gameState){
+function getDiagonalLeftToRight(gameState) {
   const diagonalLeftToRight = [];
   let count = 0;
-  for (let item of gameState.board){
-  diagonalLeftToRight.push(item[count])
-  count ++;
-}
+  for (let item of gameState.board) {
+    diagonalLeftToRight.push(item[count]);
+    count++;
+  }
   // console.log(diagonalLeftToRight);
-  return diagonalLeftToRight
-};
+  return diagonalLeftToRight;
+}
 
 /*========================================================
 ----------------------------------------------------------
 =========================================================*/
 
-function getDiagonalRightToLeft(gameState){
+function getDiagonalRightToLeft(gameState) {
   const diagonalLeftToRight = [];
   let count = 2;
-  for (let item of gameState.board){
-  diagonalLeftToRight.push(item[count])
-  count --;
-}
+  for (let item of gameState.board) {
+    diagonalLeftToRight.push(item[count]);
+    count--;
+  }
   // console.log(diagonalLeftToRight);
-  return diagonalLeftToRight
-};
+  return diagonalLeftToRight;
+}
 
 /*========================================================
 ----------------------------------------------------------
 =========================================================*/
 
-function allCharacterAreEqual(gameArray){
-  const areAllEqual = gameArray.every(setOfChars => setOfChars === gameArray[1]);
+function allCharacterAreEqual(gameArray) {
+  const areAllEqual = gameArray.every(
+    (setOfChars) => setOfChars === gameArray[1]
+  );
   // console.log(areAllEqual);
-  return areAllEqual
-
+  return areAllEqual;
 }
 
 /*========================================================
@@ -81,41 +94,80 @@ function allCharacterAreEqual(gameArray){
 // Validating Functions
 
 // checking all the rows
-function checkAllChars(gameState){
-for(let i = 0; i<gameState.board.length; i++){
-  const eachRow = getRow(gameState, i)
-  const checkEachRow = allCharacterAreEqual(eachRow);
+function checkAllRowsAndColumns(gameState) {
+  for (let i = 0; i < gameState.board.length; i++) {
+    const eachRow = getRow(gameState, i);
+    const checkEachRow = allCharacterAreEqual(eachRow);
 
-  const eachColumn = getColumn(gameState, i);
-  const checkEachColumn = allCharacterAreEqual(eachColumn)
+    const eachColumn = getColumn(gameState, i);
+    const checkEachColumn = allCharacterAreEqual(eachColumn);
 
-  if(checkEachRow || checkEachColumn){
-    console.log('gameOver');
-    return true
+    if (checkEachRow || checkEachColumn) {
+      console.log('gameOver');
+      return true;
+    }
   }
+  return false;
 }
-return false
- 
-}
-
 
 /*========================================================
 ----------------------------------------------------------
 =========================================================*/
 
-function checkAllDiagonals(gameState){
+function checkAllDiagonals(gameState) {
   const topLeft = getDiagonalLeftToRight(gameState);
   const topRight = getDiagonalRightToLeft(gameState);
   const checkTopLeft = allCharacterAreEqual(topLeft);
   const checkTopRight = allCharacterAreEqual(topRight);
-  
-if(checkTopLeft || checkTopRight){
-  console.log('game over');
-  return true;
-}else{
-  return false
+
+  if (checkTopLeft || checkTopRight) {
+    console.log('game over');
+    return true;
+  } else {
+    return false;
+  }
 }
 
+/*========================================================
+----------------------------------------------------------
+=========================================================*/
+// VARIABLES AND DOM MANIPULATION.
+
+const board = document.querySelector('.game');
+const spaces = document.querySelectorAll('.eachSpace');
+const boardArray = [...spaces];
+
+const players = ['X', 'O'];
+
+let initialPlayer = players[0];
+
+board.addEventListener('click', (e) => {
+  if (
+    initialPlayer === players[1] &&
+    e.target.innerText !== initialPlayer[0] &&
+    e.target.innerText === ''
+  ) {
+    e.target.innerText = initialPlayer;
+    initialPlayer = players[0];
+  }
+
+  if (e.target.innerText === '') {
+    e.target.innerText = initialPlayer;
+    initialPlayer = players[1];
+  }
+});
+
+// function top convert div element into board game
+
+function convertElementsIntoBoardArray(arrayHtmlElements) {
+  const board = [];
+  const arrayFromNodeList = [...arrayHtmlElements];
+  for (let i = 0; i < arrayFromNodeList.length; i += 3) {
+    const rowDivs = arrayFromNodeList.slice(i, i + 3);
+    board.push(rowDivs);
+  }
+  console.log(board);
+  return board;
 }
 
-checkAllDiagonals(gameState);
+convertElementsIntoBoardArray(spaces);
